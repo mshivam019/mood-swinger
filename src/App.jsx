bb61835ed480c87a1108466190b35b23039fb681
mshivam019 <mshivam019@gmail.com>
2023-04-01 08:15:07 +0530

    change routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -8,14 +8,14 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       <ToastContainer theme="colored" position="bottom-center" limit={1} />
     </div>
   );
