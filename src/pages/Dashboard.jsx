642f265ae9807a350fa71184ed2a54aa3d09fef6
Shivam Mishra <mshivam019@gmail.com>
2023-04-03 02:28:35 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -184,7 +184,7 @@ function Dashboard() {
         <div className="w-full h-screen hidden sm:block sm:w-20 xl:w-60 flex-shrink-0">
           .
         </div>
-        <div className=" h-screen flex-grow overflow-x-hidden overflow-y-hidden flex flex-wrap content-start px-2">
+        <div className=" h-screen flex-grow overflow-x-hidden flex flex-wrap content-start px-2">
           <div className="w-full sm:flex p-2 items-end">
             <div className="sm:flex-grow flex justify-between">
               <div className="">
